11e7a6e85cc6c5c9a91d7d1bce10f862e8af95ef
freedom_twin <dimon.twin@gmail.com>
2021-04-26 10:25:51 +0500

    datepicker

--- src/app/pages/application/shared/components/step1/step1.component.ts
+++ src/app/pages/application/shared/components/step1/step1.component.ts
@@ -1,10 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {
-  IAngularMyDpOptions,
-  IMyDateModel,
-} from 'src/app/components/angular-mydatepicker/src/public-api';
-
 declare var $: any;
 
 @Component({
@@ -16,86 +11,60 @@ export class Step1Component implements OnInit {
   form!: FormGroup;
   submitted = false;
 
-  myDpOptions: IAngularMyDpOptions = {
-    dateRange: false,
-    dateFormat: 'dd.mm.yyyy',
-    // other options are here...
-  };
-
-  myDateInit: boolean = true;
-  model!: IMyDateModel;
-
   constructor() {}
 
   ngOnInit(): void {
-    // $('.outDocDate').datepicker({
-    //   // minDate: new Date(),
-    //   inline: false,
-    //   todayButton: new Date(),
-    //   autoClose: true,
-    //   dateFormat: 'dd.mm.yyyy',
-    //   navTitles: {
-    //     days: 'MM, <span>yyyy</span>',
-    //     months: 'yyyy',
-    //     years: 'yyyy1 - yyyy2',
-    //   },
-    //   // timepicker: true,
-    //   // timeFormat: 'hh:ii AA',
-    //   onSelect: function onSelect(
-    //     formattedDate: string,
-    //     date: Date | [],
-    //     inst: object
-    //   ): void {
-    //     setDate(date);
-    //     // this.date = formattedDate;
-    //   },
-    // });
-    // const setDate = (date: Date | []) => {
-    //   this.form.patchValue({ outDocDate: date });
-    // };
+    $('.outDocDate').datepicker({
+      // minDate: new Date(),
+      inline: false,
+      todayButton: new Date(),
+      autoClose: true,
+      dateFormat: 'dd.mm.yyyy',
+      navTitles: {
+        days: 'MM, <span>yyyy</span>',
+        months: 'yyyy',
+        years: 'yyyy1 - yyyy2',
+      },
+      // timepicker: true,
+      // timeFormat: 'hh:ii AA',
+      onSelect: function onSelect(
+        formattedDate: string,
+        date: Date | [],
+        inst: object
+      ): void {
+        setDate(date);
+        // this.date = formattedDate;
+      },
+    });
+    const setDate = (date: Date | []) => {
+      this.form.patchValue({ outDocDate: date });
+    };
 
-    // $('.criminalCasecDate').datepicker({
-    //   // minDate: new Date(),
-    //   inline: false,
-    //   todayButton: new Date(),
-    //   autoClose: true,
-    //   dateFormat: 'dd.mm.yyyy',
-    //   navTitles: {
-    //     days: 'MM, <span>yyyy</span>',
-    //     months: 'yyyy',
-    //     years: 'yyyy1 - yyyy2',
-    //   },
-    //   // timepicker: true,
-    //   // timeFormat: 'hh:ii AA',
-    //   onSelect: function onSelect(
-    //     formattedDate: string,
-    //     date: Date | [],
-    //     inst: object
-    //   ): void {
-    //     setDate2(date);
-    //     // this.date = formattedDate;
-    //   },
-    // });
-    // const setDate2 = (date: Date | []) => {
-    //   this.form.patchValue({ criminalCasecDate: date });
-    // };
-
-    if (this.myDateInit) {
-      // Initialize to specific date (14.05.2019) with IMyDate object
-      this.model = {
-        isRange: false,
-        singleDate: {
-          date: {
-            year: 2019,
-            month: 5,
-            day: 14,
-          },
-        },
-      };
-    } else {
-      // Initialize to today with javascript date object
-      this.model = { isRange: false, singleDate: { jsDate: new Date() } };
-    }
+    $('.criminalCasecDate').datepicker({
+      // minDate: new Date(),
+      inline: false,
+      todayButton: new Date(),
+      autoClose: true,
+      dateFormat: 'dd.mm.yyyy',
+      navTitles: {
+        days: 'MM, <span>yyyy</span>',
+        months: 'yyyy',
+        years: 'yyyy1 - yyyy2',
+      },
+      // timepicker: true,
+      // timeFormat: 'hh:ii AA',
+      onSelect: function onSelect(
+        formattedDate: string,
+        date: Date | [],
+        inst: object
+      ): void {
+        setDate2(date);
+        // this.date = formattedDate;
+      },
+    });
+    const setDate2 = (date: Date | []) => {
+      this.form.patchValue({ criminalCasecDate: date });
+    };
 
     this.form = new FormGroup({
       numberLetter: new FormControl(null, [Validators.required]),
