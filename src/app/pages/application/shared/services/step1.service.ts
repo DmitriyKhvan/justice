3290bb1a8f761f35ea70d633b73c00529590b52a
freedom_twin <dimon.twin@gmail.com>
2021-05-11 16:08:09 +0500

    error message login

--- src/app/pages/application/shared/services/step1.service.ts
+++ src/app/pages/application/shared/services/step1.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class Step1Service {
   constructor(private http: HttpClient) {}
 
-  submit(data): Observable<any> {
-    return this.http.post().pipe();
+  submit(): Observable<any> {
+    return this.http.get(`${environment.dbUrl}/user/users`).pipe();
   }
 }
