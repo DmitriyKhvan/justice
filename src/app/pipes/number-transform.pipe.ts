c547b1f1b9e37f01fb051188cf4b4fa5f993f719
Dmitriy Khvan <dimon.twin@gmail.com>
2022-04-28 12:13:19 +0500

    edit format currency tein

--- src/app/pipes/number-transform.pipe.ts
+++ src/app/pipes/number-transform.pipe.ts
@@ -4,9 +4,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'numberTransform',
 })
 export class NumberTransformPipe implements PipeTransform {
-  transform(value: any): string {
+  transform(value: any, isTein: boolean = true): string {
     if (value) {
-      return new Intl.NumberFormat().format(value.replace(/[^0-9.]/gim, ''));
+      if (isTein) {
+        return new Intl.NumberFormat('ru-RU', {
+          minimumFractionDigits: 2,
+        }).format(value.replace(/[^0-9.]/gim, '') / 100);
+      } else {
+        return new Intl.NumberFormat('ru-RU').format(
+          value.replace(/[^0-9.]/gim, '')
+        );
+      }
     } else {
       return '';
     }
