60ae539025893f71827275f85c4a35b0062a93cb
freedom_twin <dimon.twin@gmail.com>
2023-01-18 00:40:00 +0500

    commit from home the same last commit

--- src/app/services/dictionfries.service.ts
+++ src/app/services/dictionfries.service.ts
@@ -45,8 +45,6 @@ export class DictionariesService {
   }
 
   getDicByActionId(id: number): Observable<any> {
-    debugger;
-
     return this.http
       .get(`${environment.dbUrlBek}/references/getSprByActionId?actionId=${id}`)
       .pipe(
